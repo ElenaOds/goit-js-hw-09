09d42b4091cd2d46b895a833f0e66d1aaafb2065
Elena Pedko <elpg.ods@gmail.com>
2022-11-06 20:08:49 +0200

    Task 1 is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  startBtn.addEventListener('click', () => {
+    interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+   
+  })
+
+  stopBtn.addEventListener ('click', () => {
+    clearInterval(interval);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  })
+
 
