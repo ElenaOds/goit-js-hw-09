2d5bd24075ce42d5a73b2ca52d45f4e34587ba05
Elena Pedko <elpg.ods@gmail.com>
2022-11-08 21:22:38 +0200

    bug is fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,8 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+let interval = null;
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
